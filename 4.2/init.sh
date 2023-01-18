18e7ff44baa680530ed02e927654d8328a2e5b90
Gilthonwe Apps <gilthonweapps@users.noreply.github.com>
2023-01-18 18:59:14 +0100

    Update init.sh

--- 4.2/init.sh
+++ 4.2/init.sh
@@ -53,13 +53,13 @@ fi
 
 
 # if we use a bind mount then the PG directory is empty and we have to create it
-if [ ! -f /var/lib/postgresql/14/main/PG_VERSION ]; then
-  chown postgres /var/lib/postgresql/14/main
-  sudo -u postgres /usr/lib/postgresql/14/bin/initdb -D /var/lib/postgresql/14/main
+if [ ! -f /var/lib/postgresql/15/main/PG_VERSION ]; then
+  chown postgres /var/lib/postgresql/15/main
+  sudo -u postgres /usr/lib/postgresql/15/bin/initdb -D /var/lib/postgresql/15/main
 fi
 
 # temporarily enable unsafe import optimization config
-cp /etc/postgresql/14/main/conf.d/postgres-import.conf.disabled /etc/postgresql/14/main/conf.d/postgres-import.conf
+cp /etc/postgresql/15/main/conf.d/postgres-import.conf.disabled /etc/postgresql/15/main/conf.d/postgres-import.conf
 
 sudo service postgresql start && \
 sudo -E -u postgres psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='nominatim'" | grep -q 1 || sudo -E -u postgres createuser -s nominatim && \
